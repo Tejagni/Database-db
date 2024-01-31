484feee77f10588a030504f5234e805feb10c68b
Tejagni <145690822+Tejagni@users.noreply.github.com>
2024-01-30 19:29:25 -0500

    Create work.js

---
+++ work.js
@@ -0,0 +1,14 @@
+ db.classwork.insertOne({
+    "Emp_ID": "10025AE336",
+    "Personal_details": {
+        "First_Name": "Radhika",
+        "Last_Name": "Sharma",
+        "Date_Of_Birth": "1995-09-26"
+    },
+    "Contact": {
+        "e-mail": "radhika_sharma.123@gmail.com",
+        "phone": "9848022338"
+    },
+    "Address": {
+        "city": "Hyderabad",
+        "Area": "Madapur",}})
