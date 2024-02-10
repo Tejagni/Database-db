5c001c0f47551e43b718f248a89b43de0f8d296b
Tejagni <145690822+Tejagni@users.noreply.github.com>
2024-02-09 19:35:53 -0500

    Create i.js

---
+++ i.js
@@ -0,0 +1,13 @@
+//db.books.insertOne({
+  title: "Deep Dive into React Hooks",
+  ISBN: "000000000",
+  thumbnailUrl: "",
+  publicationDate: ISODate("2019-01-01T00:00:00.000Z"),
+  authors: ["Ada Lovelace"],
+});
+
+//Output
+{
+  acknowledged: true,
+  insertedId: ObjectId('65c3adfc6f3fe4b2d83ab079')
+}
