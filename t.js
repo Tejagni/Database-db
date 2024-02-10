14a127427bffd38a17d1a50bd3e4ca9e339acfc1
Tejagni <145690822+Tejagni@users.noreply.github.com>
2024-02-09 19:37:08 -0500

    Create t.js

---
+++ t.js
@@ -0,0 +1,11 @@
+db.books.replaceOne(
+  {_id: ObjectId("65c3adfc6f3fe4b2d83ab079")},
+  {
+    _id: ObjectId("65c3adfc6f3fe4b2d83ab079"),
+    title: "Deep Dive into React Hooks",
+    ISBN: "0-3182-1299-4",
+    thumbnailUrl: "http://via.placeholder.com/640x360",
+    publicationDate: ISODate("2022-07-28T02:20:21.000Z"),
+    authors: ["Ada Lovelace"],
+  }
+);
